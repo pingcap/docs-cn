2d9f320d10b7361a2f343f6d72a4c06f7efc96a4
Grace Cai <qqzczy@126.com>
2022-02-26 10:49:44 +0800

    Update the script for getting duplicated release note  (#7705)

--- scripts/get-duplicated-notes.py
+++ scripts/get-duplicated-notes.py
@@ -1,5 +1,5 @@
-# This script compares an on-going release notes file with a previously published release notes file.
-# If the ongoing release notes file has a duplicate note with the published one, the script reports the note and replaces it with the published one. 
+# This script compares an on-going release notes file with published release notes files.
+# If the ongoing release notes file has a duplicate note with the published one, the script reports the note and replaces it with the published one.
 
 import re, os
 from tempfile import mkstemp
@@ -7,27 +7,41 @@ from shutil import move
 from os import remove
 
 
-# 获取已发布的 release notes Issue 号
-def store_exst_rn(path):
+# 获取已发布的 release notes Issue 号和 PR 号
+def store_exst_rn(ext_path,main_path):
+
     exst_notes = []
-    flag = 0
-    with open(path,'r', encoding='utf-8') as fp:
-        for line in fp:
-            exst_issue_num = re.search('\[#\d{4,5}\]', line)
-            if exst_issue_num:
-                flag = 1
-                note_pair = [exst_issue_num.group(), line]
-                exst_notes.append(note_pair)
+    exst_issue_nums = []
+
+    for maindir, subdir, files in os.walk(ext_path):
+        for afile in files:
+            file_path = (os.path.join(maindir, afile))
+            if file_path.endswith('.md') and not os.path.samefile(file_path,main_path):
+                with open(file_path,'r', encoding='utf-8') as fp:
+                    for line in fp:
+                        exst_issue_num = re.search(r'https://github.com/(pingcap|tikv)/\w+/(issues|pull)/\d+', line)
+                        if exst_issue_num:
+                            if exst_issue_num.group() not in exst_issue_nums:
+                                note_pair = [exst_issue_num.group(),line,afile]
+                                exst_issue_nums.append(exst_issue_num.group())
+                                exst_notes.append(note_pair)
+                            else:
+                                continue
+                        else:
+                            continue
             else:
-                continue
-        if flag == 1:
-            return exst_notes
-        else:
-            return 0
+                pass
+
+    if len(exst_issue_nums) != 0:
+        return exst_notes
+    else:
+        return 0
 
 
-# 检查当前准备中的 release notes 的 Issue 号是否有重复，如果有就进行替换
-def check_exst_rn(ext_path, main_path):
+# 检查当前准备中的 release notes 的 Issue 号和 PR 号是否有重复，如果有就进行替换
+def check_exst_rn(note_pairs, main_path):
+    DupNum = 0
+    NoteNum = 0
     target_file_path = mkstemp()[1]
     source_file_path = main_path
     with open(target_file_path, 'w', encoding='utf-8') as target_file:
@@ -35,21 +49,29 @@ def check_exst_rn(ext_path, main_path):
             LineNum = 0
             for line in source_file:
                 LineNum += 1
-                issue_num = re.search('\[#\d{4,5}\]', line)
+                issue_num = re.search('https://github.com/(pingcap|tikv)/\w+/(issues|pull)/\d+', line)
                 if issue_num:
-                     for note_pairs in store_exst_rn(ext_path):
-                        if issue_num.group() == note_pairs[0]:
-                            print('A duplicated note is found in line ' + str(LineNum) + note_pairs[1])
-                            line = re.sub(r'-.*$', '(dup) ' + note_pairs[1].strip(), line)
+                     NoteNum +=1
+                     for note_pair in note_pairs:
+                        if issue_num.group() == note_pair[0] and "(dup)" not in line:
+                            print('A duplicated note is found in line ' + str(LineNum) + " from " + note_pair[2] + note_pair[1])
+                            line = re.sub(r'-.*$', '(dup) {} '.format(note_pair[2]) + note_pair[1].strip(), line)
                             print('The duplicated note is replaced with ' + line)
+                            DupNum += 1
+                            break
+                        else:
+                            pass
                 target_file.write(line)
 
     remove(source_file_path)
     move(target_file_path, source_file_path)
+    DupRate = "%.0f%%" % (DupNum/NoteNum*100) #计算 release notes 重复率
+    print (str(DupNum) + " duplicated notes are found in " + str(NoteNum) + " notes. The duplicated rate is " + str(DupRate) + ".")
 
 
 if __name__ == "__main__":
 
-    ext_path = r'D:\GitHub\upstream\docs1\releases\release-5.0.6.md'  # 已发布的 release notes
-    main_path = r'D:\GitHub\upstream\docs1\releases\release-5.1.4.md'  # 当前正在准备的 release notes
-    check_exst_rn(ext_path, main_path)
+    ext_path = r'/Users/aaa/Documents/GitHub/githubid/docs/releases'  # 已发布的 release notes 文件夹
+    main_path = r'/Users/aaa/Documents/GitHub/githubid/docs/releases/release-5.3.1.md'  # 当前正在准备的release notes 文档路径
+    note_pairs = store_exst_rn(ext_path,main_path)
+    check_exst_rn(note_pairs, main_path)
