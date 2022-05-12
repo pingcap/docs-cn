5f5d94c758d66563c7e423e3eca713f517c2dff3
czhen <56986964+shczhen@users.noreply.github.com>
2022-05-12 13:36:19 +0800

    refine dedup logic

--- scripts/verify-duplicated-file-name.sh
+++ scripts/verify-duplicated-file-name.sh
@@ -2,13 +2,7 @@
 
 echo $PWD
 
-DuplicateFilename=$(awk -F'/' '{
-  f = $NF
-  a[f] = f in a? a[f] RS $0 : $0
-  b[f]++ }
-  END{for(x in b)
-        if(b[x]>1)
-          printf "Duplicate Filename: %s\n%s\n",x,a[x] }' <(find . ! -path '*/.git/*' ! -path '*/media/*' ! -name '_*.md' -type f ))
+DuplicateFilename=$(find . -name '*.md' ! -name '_*.md'  | rev | cut -d'/' -f1 | rev | sort | uniq -c -d)
 
 if [ -z "$DuplicateFilename" ]
 then
