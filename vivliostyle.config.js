5a6187c853344408d9c93b42161ad35bd0965ef8
Yuiham <Yuiham@users.noreply.github.com>
2024-12-10 11:06:13 +0800

    chore: add vivliostyle configs for generating pdf

---
+++ vivliostyle.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+/** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
+const vivliostyleConfig = {
+  title: 'Principia', // populated into 'publication.json', default to 'title' of the first entry or 'name' in 'package.json'.
+  author: 'Isaac Newton', // default to 'author' in 'package.json' or undefined
+  // language: 'la',
+  readingProgression: 'ltr', // reading progression direction, 'ltr' or 'rtl'.
+  size: 'A4',
+  // theme: '', // .css or local dir or npm package. default to undefined
+  image: 'ghcr.io/vivliostyle/cli:8.8.0',
+  entry: [ // **required field**
+    // 'introduction.md', // 'title' is automatically guessed from the file (frontmatter > first heading)
+    // {
+    //   path: 'epigraph.md',
+    //   title: 'おわりに', // title can be overwritten (entry > file),
+    //   theme: '@vivliostyle/theme-whatever' // theme can be set individually. default to root 'theme'
+    // },
+    // 'glossary.html' // html is also acceptable
+  ], // 'entry' can be 'string' or 'object' if there's only single markdown file
+  // entryContext: './manuscripts', // default to '.' (relative to 'vivliostyle.config.js')
+  // output: [ // path to generate draft file(s). default to '{title}.pdf'
+  //   './output.pdf', // the output format will be inferred from the name.
+  //   {
+  //     path: './book',
+  //     format: 'webpub',
+  //   },
+  // ],
+  // workspaceDir: '.vivliostyle', // directory which is saved intermediate files.
+  // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
+  // cover: './cover.png', // cover image. default to undefined.
+  // vfm: { // options of VFM processor
+  //   replace: [ // specify replace handlers to modify HTML outputs
+  //     {
+  //       // This handler replaces {current_time} to a current local time tag.
+  //       test: /{current_time}/,
+  //       match: (_, h) => {
+  //         const currentTime = new Date().toLocaleString();
+  //         return h('time', { datetime: currentTime }, currentTime);
+  //       },
+  //     },
+  //   ],
+  //   hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
+  //   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
+  // },
+};
+
+module.exports = vivliostyleConfig;
